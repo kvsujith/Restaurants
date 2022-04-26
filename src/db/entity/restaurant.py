2be9cf8e739fa6f3a5d55b6438904902ec3c1fe0
sujith <sujith7994037362@gmail.com>
2022-04-26 20:03:36 +0530

    restaurant db fields updated

--- src/db/entity/restaurant.py
+++ src/db/entity/restaurant.py
@@ -1,6 +1,6 @@
 from data import Base
 from src.utils.utils import get_indian_time
-from sqlalchemy import Column, String, Integer, DateTime
+from sqlalchemy import Column, String, Integer, DateTime,  func
 
 
 class Restaurant(Base):
@@ -9,7 +9,7 @@ class Restaurant(Base):
     id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String(40), nullable=False)
     description = Column(String(60), nullable=False)
-    created_at = Column(DateTime, default=get_indian_time(), nullable=False)
-    modified_at = Column(DateTime, onupdate=get_indian_time(), nullable=True)
+    created_at = Column(DateTime(timezone=True), server_default=func.now(), nullable=False)
+    modified_at = Column(DateTime(timezone=True), nullable=True,  server_onupdate=func.now())
     created_by = Column(String(50), nullable=False)
     modified_by = Column(String(50), nullable=True)
